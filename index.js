db8ec9d392d10955bc5b8548dd4c72d939544455
Aaron <aaron.ramharacksingh@gmail.com>
2019-08-05 19:05:40 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+ 
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+ 
+//   return gifts;
+// }
+ 
+// wrapGifts(gifts);
+
+// const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names, occasion) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++ ) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return thankYouCards;
+}
+
+writeCards(names);
+
+function countdown( countdown ) {
+    while ( countdown > 0 ) {
+      console.log( countdown );
+      countdown -= 1;
+    }
+    console.log( countdown );
+}
